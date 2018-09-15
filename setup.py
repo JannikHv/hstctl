11c5343912b3a19b081202b96fd83e34a52ef736
Jannik Hauptvogel <JannikHv@gmail.com>
2018-09-15 15:23:54 +0200

    Modulize - Add descriptions

--- setup.py
+++ setup.py
@@ -2,16 +2,20 @@
 
 from setuptools import setup
 
+
 exec(compile(
     open('hstctl/info.py', 'r').read(),
     'hstctl/info.py', 'exec'
 ))
 
+DESCRIPTION = '/etc/hosts file manager'
+LONG_DESCRIPTION = 'Hstctl lets you easily manage and structure your /etc/hosts file'
+
 setup(
     name = 'hstctl',
     version = __version__,
-    description = '',
-    long_description = '',
+    description = DESCRIPTION,
+    long_description = LONG_DESCRIPTION,
     url = 'https://github.com/JanniKHv/hstctl',
     author = __author__,
     author_email = __email__,
